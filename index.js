d61a9cbe2cd2102a6d1de63e1b3e1410ee5a490c
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-07 12:06:03 +0700

    Array Of Object Release 1 -- Haidar

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ function createObj(name,phase,gender){
 createObj('Akbar',1,'male');
 createObj('Icha',1,'female');
 console.log(arr);
+
+function getData(name){
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i].name === name){
+      return arr[i]; 
+    }  
+  } 
+}
+
+console.log(getData('Icha'));
