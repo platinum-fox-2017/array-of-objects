2b52f175d87ea3f47328f9a399a2ef8dacce715d
unknown <fatimah.azzhr@gmail.com>
2018-02-07 11:56:59 +0700

    release 0

---
+++ index.js
@@ -0,0 +1,18 @@
+var arr = [];
+
+function createObj(name, phase, gender) {
+  let obj = {};
+  obj.name = name;
+  obj.phase = phase;
+  obj. gender = gender;
+  arr.push(obj);
+}
+
+// TEST CASE
+createObj('Akbar', 1, 'male');
+createObj('Icha', 1, 'female');
+createObj('Adhit', 2, 'male');
+createObj('Tama', 2, 'male');
+createObj('Rifky', 3, 'male');
+
+console.log(arr);
