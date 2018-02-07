5f2f16fd3a7f471ab3311727e68c42baade4ac71
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-07 13:00:40 +0700

    release 1

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let arr = []
+
 function createObj(name, phase, gender) {
     //your code here...
-    return arr.push({name,phase,gender})
+    return arr.push({name, phase, gender})
 }
 
 createObj('Akbar', 1, 'male')
@@ -10,4 +11,12 @@ createObj('Adhit', 2, 'male')
 createObj('Tama', 2, 'male')
 createObj('Rifky', 3, 'male')
 
-console.log(arr)
+// console.log(arr)
+
+function getData(name) {
+    return arr.filter(value => {
+        return value.name == name
+    })
+}
+
+console.log(getData('Icha'))
