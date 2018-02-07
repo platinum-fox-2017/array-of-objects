96cff0f1c598d9c1ec0fea4a483b0e355521eb0b
agungcaproex <agung.caproex@gmail.com>
2018-02-07 15:41:48 +0700

    array-of-object release 1

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ function createObj(name,phase,gender){
 
 createObj('Akbar',1,'male')
 createObj('Icha',1,'female')
+createObj('Adhit',2,'male')
+createObj('Tama',2,'male')
+createObj('Rifky',3,'male')
 console.log(arr)
+
+function getData(name){
+  for(var i=0; i<arr.length; i++){
+    if(arr[i].Name === name){
+      return arr[i]
+    }
+  }
+}
+console.log(getData('Icha'))
