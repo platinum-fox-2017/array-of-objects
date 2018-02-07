508cd41a0f113c8eded0364a1faf873aef3cace3
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-07 11:27:54 +0700

    Add index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+var arr = []
+
+function createObj(name, phase, gender) {
+    var person = {
+        name: name,
+        phase: phase,
+        gender: gender,    
+    };
+    arr.push(person);
+}
+
+createObj('Akbar', 1, 'female');
+createObj('Icha',1,'female');
+createObj('Adhit',2,'male');
+createObj('Tama',2,'male');
+createObj('Rifky',3,'male');
+
+console.log(arr);
+// var arr = [
+//     {
+//         name: 'Akbar',
+//         phase: 1,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Icha',
+//         phase: 1,
+//         gender: 'female'
+//     }
+// ]
