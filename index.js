9ac3ea7061b109e2b09721c5148fae5bc4a2424f
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-07 11:14:16 +0700

    updated release 1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let arr = new Array;
-
+// ================================== Release 0 =================================
 // Solution 1 ============================
 function createObj(name,phase,gender){
   let newObj = new Object;
@@ -20,8 +20,23 @@ function createObj(name,phase,gender){
   arr.push(newObj);
 }
 
-
 createObj('Akbar', 1, 'male');
 createObj('Icha', 1, 'female');
+createObj('Andi', 1, 'male');
+createObj('Ida', 1, 'female');
+createObj('Fadhil', 1, 'male');
+createObj('Irma', 1, 'female');
 
+console.log("Release 0:");
 console.log(arr);
+
+
+// ================================== Release 1 =================================
+function getData(name){
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i].name==='Icha')
+      return arr[i];
+  }
+}
+console.log("\n\nRelease 1:");
+console.log(getData('Icha'));
