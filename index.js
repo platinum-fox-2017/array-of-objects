1e08a890d60ac9e1c0144a00b66c784b6adbe276
Ervan Adetya <vaanz.adetya@gmail.com>
2018-02-07 11:39:07 +0700

    Lupa Release 1

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ function createObj(name, phase, gender) {
     arr.push(person);
 }
 
+function getData(name) {
+    for(var i in arr) {
+        if(arr[i].name===name) return arr[i]
+    }
+    return "Not Found";
+}
+
 createObj('Akbar',1,'male');
-createObj('icha',1,'female');
+createObj('Icha',1,'female');
 createObj('Adhit',2,'male');
 createObj('Tama',2,'male');
 createObj('Rifky',3,'male');
 
 console.log(arr);
+console.log(getData('Icha'));
