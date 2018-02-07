a3d8f0039566d1c4a98fbd80c6228cc4ec41e9f8
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-07 11:15:51 +0700

    Release 1

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ function createObj(name, phase, gender) {
 	});
 }
 
+function getData(name) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i].name == name) return arr[i];
+	}
+}
+
 createObj('Akbar', 1, 'male');
 createObj('Icha', 1, 'female');
 createObj('Adhit', 2, 'male');
 createObj('Tama', 2, 'male');
 createObj('Rifky', 3, 'male');
 
-console.log(arr);
+// console.log(arr);
+console.log(getData('Icha'));
