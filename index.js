69f625c05e60bc09453ee7e74b2cea55241b7664
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-07 17:03:32 +0700

    PLUS RELEASE 1

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ createObj('Tama',2,'male');
 createObj('Rifky',3,'male')
 
 console.log(arr);
+
+
+function getData(name) {
+ for(var i=0;i<arr.length;i++) {
+    if(name===arr[i].name) {
+      return arr[i];
+    }
+  }
+}
+
+console.log(getData('Icha'));
