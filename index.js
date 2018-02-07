ac09347bee8c1580634313d2dab2d17aa6c0a403
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-07 11:00:30 +0700

    Release 0

---
+++ index.js
@@ -0,0 +1,17 @@
+var arr = [];
+
+function createObj(name, phase, gender) {
+	arr.push({
+		name: name,
+		phase: phase,
+		gender: gender
+	});
+}
+
+createObj('Akbar', 1, 'male');
+createObj('Icha', 1, 'female');
+createObj('Adhit', 2, 'male');
+createObj('Tama', 2, 'male');
+createObj('Rifky', 3, 'male');
+
+console.log(arr);
